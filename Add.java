466d239f3c474e43e970d4dfcab79d125f59638b
VangariBhavyasri <bhavyasrivangari7890@gmail.com>
2024-06-26 22:22:01 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,13 @@
+//sum using command line arguments
+class Add
+{
+public static void main(String args[])
+{
+int a,b,c;
+a=Integer.parseInt(args[0]);
+b=Integer.parseInt(args[1]);
+c=a+b;
+System.out.println(c);
+}
+}
+
