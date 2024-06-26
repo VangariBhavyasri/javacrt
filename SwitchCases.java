e741375d248d56568361fc427f8411d9066cbc4d
VangariBhavyasri <bhavyasrivangari7890@gmail.com>
2024-06-26 22:36:31 +0530

    Add files via upload

---
+++ SwitchCases.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class SwitchCases
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=2;
+switch(a)
+{
+case 1:
+System.out.println("Java");
+break;
+case 2:
+System.out.println("Python");
+break;
+case 3:
+System.out.println("C");
+break;
+default:
+System.out.println("Invalid");
+}
+}
+}
