cecfb204e7694edddb3d04ef6002bfeb3f9ad538
VangariBhavyasri <bhavyasrivangari7890@gmail.com>
2024-06-26 22:32:08 +0530

    Add files via upload

---
+++ Continuee.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Continuee
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int i;
+for(i=1;i<=8;i++)
+{
+System.out.println(i);
+if(i%3==0)
+continue;
+System.out.println("Java");
+System.out.println("Python");
+}
+}
+}
+
