252bf475b6c8bcda212960623e002e0f31349a1f
VangariBhavyasri <bhavyasrivangari7890@gmail.com>
2024-07-01 22:43:03 +0530

    Add files via upload

---
+++ StringConcat.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class StringConcat
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        String str="Bhavya";
+        String str1=str.concat("sri");
+         System.out.println(str);
+          System.out.println(str1);
+        
+    }
+}
