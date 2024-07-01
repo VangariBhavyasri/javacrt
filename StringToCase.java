e059fcaae310eaba74e75a1fc19ac72eb049d6b6
VangariBhavyasri <bhavyasrivangari7890@gmail.com>
2024-07-01 22:48:47 +0530

    Add files via upload

---
+++ StringToCase.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class StringToCase
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        String s1="bhavyasrI";
+          System.out.println(s1.toUpperCase());
+          System.out.println(s1.toLowerCase());
+        
+    }
+}
