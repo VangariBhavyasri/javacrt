1cd80577aa82a09f5d987c2a87754496f04d0109
VangariBhavyasri <bhavyasrivangari7890@gmail.com>
2024-07-02 00:10:52 +0530

    Add files via upload

---
+++ CompareToString.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class CompareToString{
+public static void main(String []args)
+{
+Scanner s=new Scanner(System.in);
+String s1=s.next();
+String s2=s.next();
+System.out.println(s1.compareTo(s2));
+}
+}
