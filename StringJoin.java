0a4e9acf248199b6183ce08446cd3eb77da426e4
VangariBhavyasri <bhavyasrivangari7890@gmail.com>
2024-07-02 19:16:59 +0530

    Add files via upload

---
+++ StringJoin.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class StringJoin{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String str[]={"platform","dependent","lang"};
+String str1=String.join(" ",str);
+System.out.println(str1);
+String str2=String.join(" is ",str);
+System.out.println(str2);
+String str3=String.join("",str);
+System.out.println(str3);
+}
+}
+
