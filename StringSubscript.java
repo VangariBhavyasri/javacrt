6b317a5a4e200afc131de2f49cb9fe569e241ba9
VangariBhavyasri <bhavyasrivangari7890@gmail.com>
2024-07-02 10:17:48 +0530

    Add files via upload

---
+++ StringSubscript.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class StringSubscript{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+String s1=s.next();
+String s2=s.next();
+if(s1.contains(s2))
+{
+System.out.println("subscript");
+}
+else
+{
+System.out.println("not a subscript");
+}
+}
+}
+
