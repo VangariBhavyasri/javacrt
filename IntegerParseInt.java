542b4e485bba329b8f19adb132003b6f37224da3
VangariBhavyasri <bhavyasrivangari7890@gmail.com>
2024-07-02 19:59:50 +0530

    Add files via upload

---
+++ IntegerParseInt.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class IntegerParseInt{
+public static void main(String []args){
+String s1="237";
+String s2="562";
+System.out.println(Integer.parseInt(s1)+Integer.parseInt(s2));
+}
+}
+
