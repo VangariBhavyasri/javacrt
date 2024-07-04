1db5909c6efaa0b40d5530b376cd4ded357c5cc7
VangariBhavyasri <bhavyasrivangari7890@gmail.com>
2024-07-04 22:16:18 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,19 @@
+class Sample1    //instance variables default values
+{
+    int a;
+    long b;
+    double c;
+    String d;
+}
+        class Sample
+        {
+        public static void main(String[] args)
+        {
+       Sample1 s1=new Sample1();
+       System.out.println(s1.a);
+       System.out.println(s1.b);
+       System.out.println(s1.c);
+       System.out.println(s1.d);
+        
+    }
+}
