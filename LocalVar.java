fa860e1590c39e0695d9ae89cd0ca98e63ddedb0
VangariBhavyasri <bhavyasrivangari7890@gmail.com>
2024-07-04 22:01:39 +0530

    Add files via upload

---
+++ LocalVar.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Sample
+{
+    int a=10;
+    static int b=20;
+        void show()
+        {
+            int c=30;
+             System.out.println(a);
+             System.out.println(b);
+             System.out.println(c);
+        }
+}
+        class LocalVar
+        {
+        public static void main(String[] args)
+        {
+        Scanner s=new Scanner(System.in);
+       Sample s1=new Sample();
+       s1.show();
+        
+    }
+}
